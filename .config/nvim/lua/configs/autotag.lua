21e07db1e314d89a0420a2db46432b966d2e4343
arosario513 <albertojovi4@gmail.com>
2024-08-11 10:49:26 -0400

    neovim: added treesitter(lmao)and fixed auto indent for html

---
+++ .config/nvim/lua/configs/autotag.lua
@@ -0,0 +1,12 @@
+local nvim_ts_autotag = require "nvim-ts-autotag"
+nvim_ts_autotag.setup {
+    opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_trailing = false,
+    },
+
+    aliases = {
+        ["javascriptreact"] = "html",
+    },
+}
---
+++ .config/nvim/lua/configs/treesitter.lua
@@ -0,0 +1,20 @@
+local treesitter = require "nvim-treesitter.configs"
+treesitter.setup {
+    ensure_installed = {
+        "c",
+        "cpp",
+        "c_sharp",
+        "html",
+        "css",
+        "rust",
+        "lua",
+        "javascript",
+        "ini",
+        "markdown",
+        "markdown_inline",
+    },
+    auto_install = true,
+    autotag = { enable = true },
+    highlight = { enable = true },
+    indent = { enable = true },
+}
