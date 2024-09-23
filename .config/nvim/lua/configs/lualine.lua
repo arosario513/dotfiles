268b55b9224743f2bda244eb091cdfb8e347c206
arosario513 <a.rosario51304@gmail.com>
2024-09-23 10:12:43 -0400

    changed lualine to onedark; switched terminal theme

--- .config/nvim/lua/configs/lualine.lua
+++ .config/nvim/lua/configs/lualine.lua
@@ -3,10 +3,6 @@ if not status then
     return
 end
 
-local custom_theme = require "lualine.themes.pywal"
-custom_theme.normal.a.bg = "#FF0000"
-custom_theme.normal.c.bg = "#202020"
-
 lualine.setup {
-    options = { theme = custom_theme },
+    options = { theme = "onedark" },
 }
