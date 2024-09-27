b48e976ed1cbc18312cd88a78d41a652a8dd9332
arosario513 <a.rosario51304@gmail.com>
2024-09-27 16:38:58 -0400

    added dockerls

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -2,8 +2,8 @@ local lspconfig = require "lspconfig"
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local phpactor_capabilities = vim.lsp.protocol.make_client_capabilities()
 phpactor_capabilities.textDocument.foldingRange = {
-    dynamicRegistration = false,
-    lineFoldingOnly = true,
+  dynamicRegistration = false,
+  lineFoldingOnly = true,
 }
 lspconfig.clangd.setup { capabilities = capabilities }
 lspconfig.cssls.setup { capabilities = capabilities }
@@ -15,50 +15,50 @@ lspconfig.csharp_ls.setup { capabilities = capabilities }
 lspconfig.eslint.setup { capabilities = capabilities }
 lspconfig.bashls.setup { capabilities = capabilities }
 lspconfig.pylsp.setup {
-    capabilities = capabilities,
-    settings = {
-        pylsp = {
-            pylint = {
-                enabled = true,
-                executable = "pylint",
-            },
-        },
+  capabilities = capabilities,
+  settings = {
+    pylsp = {
+      pylint = {
+        enabled = true,
+        executable = "pylint",
+      },
     },
+  },
 }
 lspconfig.jedi_language_server.setup { capabilities = capabilities }
 lspconfig.lua_ls.setup {
-    capabilities = capabilities,
-    on_init = function(client)
-        client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-            runtime = {
-                version = "LuaJIT",
-            },
-            workspace = {
-                checkThirdParty = false,
-                library = {
-                    vim.env.VIMRUNTIME,
-                },
-            },
-        })
-    end,
-    settings = {
-        Lua = {},
-    },
+  capabilities = capabilities,
+  on_init = function(client)
+    client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+      runtime = {
+        version = "LuaJIT",
+      },
+      workspace = {
+        checkThirdParty = false,
+        library = {
+          vim.env.VIMRUNTIME,
+        },
+      },
+    })
+  end,
+  settings = {
+    Lua = {},
+  },
 }
 
 lspconfig.pyright.setup { capabilities = capabilities }
 lspconfig.ts_ls.setup { capabilities = capabilities }
 lspconfig.intelephense.setup {
-    capabilities = capabilities,
-    root_dir = function(_)
-        return vim.loop.cwd()
-    end,
+  capabilities = capabilities,
+  root_dir = function(_)
+    return vim.loop.cwd()
+  end,
 }
 lspconfig.phpactor.setup {
-    capabilities = phpactor_capabilities,
-    root_dir = function(_)
-        return vim.loop.cwd()
-    end,
+  capabilities = phpactor_capabilities,
+  root_dir = function(_)
+    return vim.loop.cwd()
+  end,
 }
 
 lspconfig.java_language_server.setup { capabilities = capabilities }
@@ -66,6 +66,7 @@ lspconfig.yamlls.setup { capabilities = capabilities }
 lspconfig.cmake.setup { capabilities = capabilities }
 lspconfig.asm_lsp.setup { capabilities = capabilities }
 lspconfig.powershell_es.setup {
-    capabilities = capabilities,
-    bundle_path = vim.fn.stdpath "data" .. "/mason/packages/powershell-editor-services",
+  capabilities = capabilities,
+  bundle_path = vim.fn.stdpath "data" .. "/mason/packages/powershell-editor-services",
 }
+lspconfig.dockerls.setup { capabilities = capabilities }
