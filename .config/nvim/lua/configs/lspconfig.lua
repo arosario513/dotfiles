5b93d8bb28e3501f20444ecfb33b81f3bfe68600
arosario513 <a.rosario51304@gmail.com>
2025-05-24 15:44:53 -0400

    Added bacon and bacon-lsp

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -64,6 +64,7 @@ lspconfig.htmx.setup { capabilities = capabilities }
 lspconfig.jinja_lsp.setup { capabilities = capabilities }
 lspconfig.djlsp.setup { capabilities = capabilities }
 lspconfig.ruff.setup { capabilities = capabilities }
+lspconfig.bacon_ls.setup{ capabilities = capabilities }
 
 vim.api.nvim_create_autocmd("LspAttach", {
     group = vim.api.nvim_create_augroup("lsp_attach_disable_ruff_hover", { clear = true }),
