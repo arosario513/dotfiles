8c8d9ac0a297f32074a56a66617790c0d4933fe4
arosario513 <a.rosario51304@gmail.com>
2025-10-13 12:02:27 -0400

    Added keybinds for trouble.nvim

--- .config/nvim/lua/keybinds.lua
+++ .config/nvim/lua/keybinds.lua
@@ -2,37 +2,45 @@
 
 vim.api.nvim_set_keymap("n", "<leader>e", ":Neotree toggle<CR>", { noremap = true })
 
-vim.keymap.set("n", "<leader>l", ":Lazy<CR>", {})
-vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
-vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Goto Definition" })
-vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions" })
+local setkey = vim.keymap.set
 
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", {})
+setkey("n", "<leader>l", ":Lazy<CR>", {})
+setkey("n", "K", vim.lsp.buf.hover, { desc = "Hover" })
+setkey("n", "gd", vim.lsp.buf.definition, { desc = "Goto Definition" })
+setkey("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code Actions" })
 
-vim.keymap.set("n", "<leader>th", ":split | terminal<CR>", {})
-vim.keymap.set("n", "<leader>tv", ":vsplit | terminal<CR>", {})
+setkey("t", "<Esc>", "<C-\\><C-n>", {})
 
-vim.keymap.set("n", "gt", ":BufferNext<CR>", {})
+setkey("n", "<leader>th", ":split | terminal<CR>", {})
+setkey("n", "<leader>tv", ":vsplit | terminal<CR>", {})
+
+setkey("n", "gt", ":BufferNext<CR>", {})
 
 for i = 1, 9 do
-	vim.keymap.set("n", i .. "gt", ":BufferGoto " .. i .. "<CR>", {})
+	setkey("n", i .. "gt", ":BufferGoto " .. i .. "<CR>", {})
 end
 
-vim.keymap.set("n", "<leader>?", ":WhichKey<CR>", {})
+setkey("n", "<leader>?", ":WhichKey<CR>", {})
 
-vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
-vim.keymap.set("n", "<leader>gl", ":Gitsigns toggle_current_line_blame<CR>", {})
-vim.keymap.set("n", "<leader>gD", ":Gitsigns diffthis<CR>", {})
+setkey("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", {})
+setkey("n", "<leader>gl", ":Gitsigns toggle_current_line_blame<CR>", {})
+setkey("n", "<leader>gD", ":Gitsigns diffthis<CR>", {})
 
-vim.keymap.set("n", "<leader>s", ":source<CR>", {})
+setkey("n", "<leader>s", ":source<CR>", {})
 
-vim.keymap.set("n", "<leader>v", ":VenvSelect<CR>", {})
+setkey("n", "<leader>v", ":VenvSelect<CR>", {})
 
-vim.keymap.set("n", "<leader>m", ":Mason<CR>", {})
+setkey("n", "<leader>m", ":Mason<CR>", {})
 
-vim.keymap.set("n", "<leader>bc", ":BufferClose<CR>", {})
-vim.keymap.set("n", "<leader>bn", ":BufferMoveNext<CR>", {})
-vim.keymap.set("n", "<leader>bp", ":BufferMovePrevious<CR>", {})
-vim.keymap.set("n", "<leader>bs", ":BufferMoveStart<CR>", {})
-vim.keymap.set("n", "<leader>dd", vim.diagnostic.open_float, { desc = "Show Diagnostics" })
-vim.keymap.set("n", "<leader>dr", ":RustLsp renderDiagnostic<CR>", { desc = "Show Diagnostics (RustLsp)" })
+setkey("n", "<leader>bc", ":BufferClose<CR>", {})
+setkey("n", "<leader>bn", ":BufferMoveNext<CR>", {})
+setkey("n", "<leader>bp", ":BufferMovePrevious<CR>", {})
+setkey("n", "<leader>bs", ":BufferMoveStart<CR>", {})
+setkey("n", "<leader>dd", vim.diagnostic.open_float, { desc = "Show Diagnostics" })
+setkey("n", "<leader>dr", ":RustLsp renderDiagnostic<CR>", { desc = "Show Diagnostics (RustLsp)" })
+
+setkey("n", "<leader>xx", ":Trouble diagnostics toggle<CR>", { desc = "Show Diagnostics (Trouble)" })
+setkey("n", "<leader>xX", ":Trouble diagnostics toggle filter.buf=0<CR>", { desc = "Buffer Diagnostics (Trouble)" })
+setkey("n", "<leader>xl", ":Trouble loclist toggle<CR>", { desc = "Location List (Trouble)" })
+setkey("n", "<leader>xr", ":Trouble lsp_references toggle<CR>", { desc = "References (Trouble)" })
+setkey("n", "<leader>xs", ":Trouble symbols toggle win.position=right<CR>", { desc = "Symbols (Trouble)" })
